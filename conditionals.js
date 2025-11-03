abf49ee14bec892dc12be63ed763f12daaa9f2b4
bugsagweler-ctrl <bugsagweler@gmail.com>
2025-11-03 12:06:14 +0200

    JavaScript Exercise 4

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+// TODO: Create a variable called score and give it a value between 0 and 100
+var score = 95;
+// TODO: Create a variable called passingGrade and set it to 60
+var passingGrade = 60;
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade
+if (score >= passingGrade){
+    console.log("Perfect score");
+}
+// TODO: Add an else statement for when the score is below passingGrade
+ else  {
+    console.log("Not perfect score");
+}
+    
+// TODO: Print "Passed" or "Failed" based on the score
+ if (score >=passingGrade){
+    console.log("Passed");
+}
+else {
+    console.log("Failed");
+}
+// TODO: Add another condition to check if the score is perfect (100)
+if (score === 100){
+    console.log("The score is perfect");
+}
+else {
+    console.log("Not perfect");
+}
